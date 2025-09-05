3d1a2495a636407b95c31a7886d3f94830f508ec
jeangomes01 <jeangomes1567@gmail.com>
2025-09-05 11:13:56 -0300

    Implement theme service in AppComponent and update styles across components to use CSS variables for improved theming. Refactor About, Contact, and Projects pages to enhance visual consistency and responsiveness.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ThemeService } from './core/services/theme.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'portfolio-angular';
+
+  constructor(private themeService: ThemeService) {}
+
+  ngOnInit(): void {
+    // Garantir que o tema seja aplicado na inicialização
+    this.themeService.setTheme(this.themeService.getCurrentTheme());
+  }
 }
